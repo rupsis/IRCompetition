7bba3d0a87fc5ace2ad04e9e05432716d84c54b8
Nate Rupsis <nathaniel.rupsis@homeadvisor.com>
2020-11-23 19:38:16 -0700

    setting up basic env

---
+++ search_eval.py
@@ -0,0 +1,80 @@
+import math
+import sys
+import time
+import metapy
+import pytoml
+
+def load_ranker(cfg_file):
+    """
+    Use this function to return the Ranker object to evaluate, 
+    The parameter to this function, cfg_file, is the path to a
+    configuration file used to load the index.
+    """
+    return metapy.index.OkapiBM25(k1=1.2,b=0.75,k3=0.0111)
+
+
+def runQueries(param, print_on=True):
+    cfg = sys.argv[1]
+    # print('Building or loading index...')
+    idx = metapy.index.make_inverted_index(cfg)
+
+    
+    ranker = load_ranker_test(cfg, param)
+    ev = metapy.index.IREval(cfg)
+
+    with open(cfg, 'r') as fin:
+        cfg_d = pytoml.load(fin)
+
+    query_cfg = cfg_d['query-runner']
+    if query_cfg is None:
+        print("query-runner table needed in {}".format(cfg))
+        sys.exit(1)
+
+    start_time = time.time()
+    top_k = 10
+    query_path = query_cfg.get('query-path', 'queries.txt')
+    query_start = query_cfg.get('query-id-start', 0)
+
+    query = metapy.index.Document()
+    ndcg = 0.0
+    num_queries = 0
+
+    if print_on:
+        print('Running queries')
+    with open(query_path) as query_file:
+        for query_num, line in enumerate(query_file):
+            query.content(line.strip())
+            results = ranker.score(idx, query, top_k)
+            ndcg += ev.ndcg(results, query_start + query_num, top_k)
+            num_queries+=1
+    ndcg= ndcg / num_queries
+    
+    if print_on:
+        print("NDCG@{}: {}".format(top_k, ndcg))
+        print("Elapsed: {} seconds".format(round(time.time() - start_time, 4)))
+
+    return(ndcg, param)
+
+
+def testModel():
+
+
+    for i in range(1, 100):
+        results.append(runQueries(i, print_on=False))
+        print("{} out of {}".format(i, 10000))
+
+    results_sorted = sorted(results, reverse=True)
+    print("BM25 results: ")
+    for k in results_sorted[:100]:
+        print("{}, k = {}".format(k[0], k[1]))
+
+
+if __name__ == '__main__':
+
+    results = []
+
+    if len(sys.argv) != 2:
+        print("Usage: {} config.toml".format(sys.argv[0]))
+        sys.exit(1)
+
+    testModel()
