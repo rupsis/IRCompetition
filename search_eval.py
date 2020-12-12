2e836e546105f03212a55af4675a70df818ee80e
Nate Rupsis <nathaniel.rupsis@homeadvisor.com>
2020-12-12 09:57:07 -0700

    getting back to original bm25

--- search_eval.py
+++ search_eval.py
@@ -44,7 +44,8 @@ def load_ranker(cfg_file):
     The parameter to this function, cfg_file, is the path to a
     configuration file used to load the index.
     """
-    return metapy.index.OkapiBM25(k1=3,b=0.75, k3=1000000)
+    # return metapy.index.OkapiBM25(k1=3,b=0.75, k3=1000000)
+    return metapy.index.OkapiBM25()
     # return metapy.index.AbsoluteDiscount(0.7)
 
 
